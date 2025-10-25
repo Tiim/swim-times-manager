d8a1a6b2fb9112eb5f9a2d7e280bb7f1241d4b57
Tiim <tim.bachmann96@gmail.com>
2025-10-26 00:25:01 +0200

    Fix navigation links for GitHub Pages deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,19 +13,17 @@ import AthleteProfile from "@/pages/AthleteProfile";
 import ImportExport from "@/pages/ImportExport";
 import NotFound from "@/pages/not-found";
 
-function AppRouter() {
+function Routes() {
   return (
-    <Router base="/swim-times-manager">
-      <Switch>
-        <Route path="/" component={Dashboard} />
-        <Route path="/athletes" component={Athletes} />
-        <Route path="/all-times" component={AllTimes} />
-        <Route path="/add-time" component={AddTime} />
-        <Route path="/import-export" component={ImportExport} />
-        <Route path="/athlete/:id" component={AthleteProfile} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" component={Dashboard} />
+      <Route path="/athletes" component={Athletes} />
+      <Route path="/all-times" component={AllTimes} />
+      <Route path="/add-time" component={AddTime} />
+      <Route path="/import-export" component={ImportExport} />
+      <Route path="/athlete/:id" component={AthleteProfile} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
@@ -38,21 +36,23 @@ export default function App() {
   return (
     <TooltipProvider>
       <ThemeProvider>
-        <SidebarProvider style={style as React.CSSProperties}>
-          <div className="flex h-screen w-full">
-            <AppSidebar />
-            <div className="flex flex-col flex-1 overflow-hidden">
-              <header className="flex items-center justify-between p-3 border-b border-border bg-background sticky top-0 z-10">
-                <SidebarTrigger data-testid="button-sidebar-toggle" />
-                <ThemeToggle />
-              </header>
-              <main className="flex-1 overflow-auto p-6">
-                <AppRouter />
-              </main>
+        <Router base="/swim-times-manager">
+          <SidebarProvider style={style as React.CSSProperties}>
+            <div className="flex h-screen w-full">
+              <AppSidebar />
+              <div className="flex flex-col flex-1 overflow-hidden">
+                <header className="flex items-center justify-between p-3 border-b border-border bg-background sticky top-0 z-10">
+                  <SidebarTrigger data-testid="button-sidebar-toggle" />
+                  <ThemeToggle />
+                </header>
+                <main className="flex-1 overflow-auto p-6">
+                  <Routes />
+                </main>
+              </div>
             </div>
-          </div>
-        </SidebarProvider>
-        <Toaster />
+          </SidebarProvider>
+          <Toaster />
+        </Router>
       </ThemeProvider>
     </TooltipProvider>
   );
